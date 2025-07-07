747608cf7c8b02e4342df9fe4e63808922886fb9
arth1312 <arthkoradiya@gmail.com>
2025-07-07 19:11:48 +0530

    p-5_Bootstrap-Integration-Templete 07/07/2025

--- pr-5_Bootstrap-Integration-templete/src/Components/Aboutcustomer/Aboutcusto.jsx
+++ pr-5_Bootstrap-Integration-templete/src/Components/Aboutcustomer/Aboutcusto.jsx
@@ -1,114 +1,95 @@
 import { Swiper, SwiperSlide } from 'swiper/react';
+import { Autoplay, Pagination } from 'swiper/modules';
 import 'swiper/css';
 import 'swiper/css/pagination';
-import { Pagination } from 'swiper/modules';
 
-import './Aboutcusto.css'
+import './Aboutcusto.css';
 import { Container } from 'react-bootstrap';
-import customertop from '../../assets/images/customer-top-bg.png'
-import customerbottom from '../../assets/images/customer-bottom-bg.png'
-import customer from '../../assets/images/customer.png'
+
+import customertop from '../../assets/images/customer-top-bg.png';
+import customerbottom from '../../assets/images/customer-bottom-bg.png';
+import customer from '../../assets/images/customer.png';
+
+const testimonials = [
+    {
+        name: "JOHN DOE",
+        role: "Designer",
+        image: customer,
+        text: [
+            "The one day when the lady met this fellow and they knew it was much more than a hunch the first mate and his Skipper too will do their comforta The one day when the lady met this fellow and they knew it was much more than a hunch the first mate and his Skipper too will do their comforta The one day when the lady met this fellow and they knew it was much "
+        ]
+    },
+    {
+        name: "JOHN DOE",
+        role: "Designer",
+        image: customer,
+        text: [
+            "The one day when the lady met this fellow and they knew it was much more than a hunch the first mate and his Skipper too will do their comforta The one day when the lady met this fellow and they knew it was much more than a hunch the first mate and his Skipper too will do their comforta The one day when the lady met this fellow and they knew it was much "
+        ]
+    },
+    {
+        name: "JOHN DOE",
+        role: "Designer",
+        image: customer,
+        text: [
+            "The one day when the lady met this fellow and they knew it was much more than a hunch the first mate and his Skipper too will do their comforta The one day when the lady met this fellow and they knew it was much more than a hunch the first mate and his Skipper too will do their comforta The one day when the lady met this fellow and they knew it was much "
+        ]
+    }
+    
+];
 
 const AboutCustomer = () => {
     return (
-        <>
-            <div className="about-custo position-relative">
-                <Container>
-                    <div className="customer-inner">
-                        <div className="customer-top-bg">
-                            <img src={customertop} alt="" />
-                        </div>
-                        <div className="customer-heading text-center">
-                            <p className='mb-0'>What Say Our Clients</p>
-                            <h2 className='text-uppercase text-white mb-5'>Customer Reviews</h2>
-                        </div>
-                        <div className="customer-slide">
-                            <Swiper
-                                direction={'vertical'}
-                                pagination={{
-                                    clickable: true,
-                                }}
-                                modules={[Pagination]}
-                                className="mySwiper"
-                            >
-                                <SwiperSlide>
-                                    <div className="testimonial-slide">
-                                        <div className="testimonial-image-box">
-                                            <img src={customer} alt="John Doe" className="testimonial-image" />
-                                            <h2 className="testimonial-name">JOHN DOE</h2>
-                                        </div>
-                                        <div className="testimonial-content">
-                                            <span className='square'></span>
-                                            <p className="testimonial-text mb-0">
-                                                The one day when the lady met this fellow and they knew it was much more than a hunch the first mate and his Skipper too will do their comforta
-                                            </p>
-                                            <p className="testimonial-text mb-0">
-                                                The one day when the lady met this fellow and they knew it was much more than a hunch the first mate and his Skipper too will do their comforta
-                                            </p>
-                                            <p className="testimonial-text mb-0">
-                                                The one day when the lady met this fellow and they knew it was much
-                                            </p>
-                                            <p className="testimonial-footer mb-0">
-                                                JOHN DOE <span className='mb-0'>- designer</span>
-                                            </p>
-                                        </div>
-                                    </div>
-                                </SwiperSlide>
-                                <SwiperSlide>
-                                    <div className="testimonial-slide">
-                                        <div className="testimonial-image-box">
-                                            <img src={customer} alt="John Doe" className="testimonial-image" />
-                                            <h2 className="testimonial-name">JOHN DOE</h2>
-                                        </div>
-                                        <div className="testimonial-content">
-                                            <span className='square'></span>
-                                            <p className="testimonial-text mb-0">
-                                                The one day when the lady met this fellow and they knew it was much more than a hunch the first mate and his Skipper too will do their comforta
-                                            </p>
-                                            <p className="testimonial-text mb-0">
-                                                The one day when the lady met this fellow and they knew it was much more than a hunch the first mate and his Skipper too will do their comforta
-                                            </p>
-                                            <p className="testimonial-text mb-0">
-                                                The one day when the lady met this fellow and they knew it was much
-                                            </p>
-                                            <p className="testimonial-footer mb-0">
-                                                JOHN DOE <span className='mb-0'>- designer</span>
-                                            </p>
-                                        </div>
-                                    </div>
-                                </SwiperSlide>
-                                <SwiperSlide>
-                                    <div className="testimonial-slide">
-                                        <div className="testimonial-image-box">
-                                            <img src={customer} alt="John Doe" className="testimonial-image" />
-                                            <h2 className="testimonial-name">JOHN DOE</h2>
-                                        </div>
-                                        <div className="testimonial-content">
-                                            <span className='square'></span>
-                                            <p className="testimonial-text mb-0">
-                                                The one day when the lady met this fellow and they knew it was much more than a hunch the first mate and his Skipper too will do their comforta
-                                            </p>
-                                            <p className="testimonial-text mb-0">
-                                                The one day when the lady met this fellow and they knew it was much more than a hunch the first mate and his Skipper too will do their comforta
-                                            </p>
-                                            <p className="testimonial-text mb-0">
-                                                The one day when the lady met this fellow and they knew it was much
-                                            </p>
-                                            <p className="testimonial-footer mb-0">
-                                                JOHN DOE <span className='mb-0'>- designer</span>
-                                            </p>
-                                        </div>
-                                    </div>
-                                </SwiperSlide>
-                            </Swiper>
-                        </div>
-                        <div className="customer-bottom-bg">
-                            <img src={customerbottom} alt="" />
-                        </div>
+        <div className="about-custo position-relative">
+            <Container>
+                <div className="customer-inner">
+                    <div className="customer-top-bg">
+                        <img src={customertop} alt="Top Background" />
                     </div>
-                </Container>
-            </div>
-        </>
-    )
-}
+
+                    <div className="customer-heading text-center">
+                        <p className='mb-0'>What Say Our Clients</p>
+                        <h2 className='text-uppercase text-white mb-5'>Customer Reviews</h2>
+                    </div>
+
+                    <div className="customer-slide">
+                        <Swiper
+                            direction="vertical"
+                            loop={true}
+                            autoplay={{ delay: 4000, disableOnInteraction: false }}
+                            pagination={{ clickable: true }}
+                            modules={[Autoplay, Pagination]}
+                            className="mySwiper"
+                        >
+                            {testimonials.map((item, index) => (
+                                <SwiperSlide key={index}>
+                                    <div className="testimonial-slide">
+                                        <div className="testimonial-image-box">
+                                            <img src={item.image} alt={item.name} className="testimonial-image" />
+                                            <h2 className="testimonial-name">{item.name}</h2>
+                                        </div>
+                                        <div className="testimonial-content">
+                                            <span className='square'></span>
+                                            {item.text.map((para, i) => (
+                                                <p className="testimonial-text mb-0" key={i}>{para}</p>
+                                            ))}
+                                            <p className="testimonial-footer mb-0">
+                                                {item.name} <span>- {item.role}</span>
+                                            </p>
+                                        </div>
+                                    </div>
+                                </SwiperSlide>
+                            ))}
+                        </Swiper>
+                    </div>
+
+                    <div className="customer-bottom-bg">
+                        <img src={customerbottom} alt="Bottom Background" />
+                    </div>
+                </div>
+            </Container>
+        </div>
+    );
+};
+
 export default AboutCustomer;
--- pr-5_Bootstrap-Integration-templete/src/Components/Header/Header.jsx
+++ pr-5_Bootstrap-Integration-templete/src/Components/Header/Header.jsx
@@ -5,59 +5,58 @@ import { NavDropdown } from 'react-bootstrap';
 import logo from '../../assets/images/header.png';
 import { IoCallOutline } from "react-icons/io5";
 import { HiOutlineShoppingBag } from "react-icons/hi";
-import './Header.css'
-
+import './Header.css';
 
 const Header = () => {
     return (
-
-
-        <Navbar expand="lg" className="sticky-top" style={{ backgroundColor: '#121619' }}>
-            <Container fluid className="d-flex justify-content-between align-items-center px-4" style={{ maxWidth: '1200px' }}>
-
+        <Navbar expand="lg" className="sticky-top main-navbar" style={{ backgroundColor: '#121619' }}>
+            <Container fluid className=" navbar d-flex justify-content-between align-items-center px-4" style={{ maxWidth: '1200px' }}>
                 <Navbar.Brand href="#">
                     <img src={logo} alt="header-logo" width={150} />
                 </Navbar.Brand>
 
-                <Nav className="index-header">
-                    <Nav.Link href="#home" className="nav-link" style={{ color: 'white', fontSize: '16px' }}>Home</Nav.Link>
-                    <NavDropdown title="Menu" id="menu-dropdown" className="nav-link-custom dropdown-custom">
-                        <NavDropdown.Item href="#about-us">Menu list</NavDropdown.Item>
-                        <NavDropdown.Item href="#contact">Menu grid</NavDropdown.Item>
-                        <NavDropdown.Item href="#shop-grid">Special Pizza</NavDropdown.Item>
-                        <NavDropdown.Item href="#404">All pizza</NavDropdown.Item>
-                    </NavDropdown>
+                <Navbar.Toggle aria-controls="navbarResponsive" className="navbar-toggler-custom " />
 
-                    {/* <Nav.Link href="#menu" className="nav-link" style={{ color: 'white' , fontSize:'16px'}}>Menu</Nav.Link> */}
+   
 
-                    <NavDropdown title="Blog" id="blog-dropdown" className="nav-link-custom dropdown-custom">
-                        <NavDropdown.Item href="#blog-left">Blog Leftside</NavDropdown.Item>
-                        <NavDropdown.Item href="#blog-right">Blog Rightside</NavDropdown.Item>
-                        <NavDropdown.Item href="#blog-detail">Blog Detail</NavDropdown.Item>
-                    </NavDropdown>
-                    <Nav.Link href="#reservation" className="nav-link" style={{ color: 'white', fontSize: '16px' }}>Reservation</Nav.Link>
+                <Navbar.Collapse id="navbarResponsive">
+                    <Nav className="index-header ms-auto text-center">
+                        <Nav.Link href="#home" className="nav-link">Home</Nav.Link>
 
-                    <NavDropdown title="Pages" id="pages-dropdown" className="nav-link-custom dropdown-custom">
-                        <NavDropdown.Item href="#about-us">About Us</NavDropdown.Item>
-                        <NavDropdown.Item href="#contact">Contact</NavDropdown.Item>
-                        <NavDropdown.Item href="#shop-grid">Shop Grid</NavDropdown.Item>
-                        <NavDropdown.Item href="#404">404</NavDropdown.Item>
-                    </NavDropdown>
-                </Nav>
+                        <NavDropdown title="Menu" id="menu-dropdown" className="nav-link-custom dropdown-custom">
+                            <NavDropdown.Item href="#menu-list">Menu list</NavDropdown.Item>
+                            <NavDropdown.Item href="#menu-grid">Menu grid</NavDropdown.Item>
+                            <NavDropdown.Item href="#special-pizza">Special Pizza</NavDropdown.Item>
+                            <NavDropdown.Item href="#all-pizza">All pizza</NavDropdown.Item>
+                        </NavDropdown>
 
+                        <NavDropdown title="Blog" id="blog-dropdown" className="nav-link-custom dropdown-custom">
+                            <NavDropdown.Item href="#blog-left">Blog Leftside</NavDropdown.Item>
+                            <NavDropdown.Item href="#blog-right">Blog Rightside</NavDropdown.Item>
+                            <NavDropdown.Item href="#blog-detail">Blog Detail</NavDropdown.Item>
+                        </NavDropdown>
 
-                <div className="d-flex align-items-center gap-3">
-                    <div className="d-flex align-items-center text-white phone-icon-hover">
+                        <Nav.Link href="#reservation" className="nav-link">Reservation</Nav.Link>
+
+                        <NavDropdown title="Pages" id="pages-dropdown" className="nav-link-custom dropdown-custom">
+                            <NavDropdown.Item href="#about-us">About Us</NavDropdown.Item>
+                            <NavDropdown.Item href="#contact">Contact</NavDropdown.Item>
+                            <NavDropdown.Item href="#shop-grid">Shop Grid</NavDropdown.Item>
+                            <NavDropdown.Item href="#404">404</NavDropdown.Item>
+                        </NavDropdown>
+                    </Nav>
+                </Navbar.Collapse>
+
+                <div className="d-flex align-items-center gap-3 icon-box">
+                    <div className="d-flex align-items-center text-white phone-icon-hover contact-info">
                         <IoCallOutline className="me-2 header-icon" />
-                        +91 123 456 789
+                        <span className="contact-text">+91 123 456 789</span>
                     </div>
-                    <div className="d-flex align-items-center text-white">
+                    <div className="d-flex align-items-center text-white bag-icon-hover contact-info">
                         <HiOutlineShoppingBag className="me-2 header-icon" />
-                        0 items - <span className="ms-1">$0.00</span>
+                        <span className="contact-text">0 items - <span className="ms-1">$0.00</span></span>
                     </div>
-                    <button className="order-button">
-                        ORDER ONLINE
-                    </button>
+                    <button className="order-button d-none d-md-inline">ORDER ONLINE</button>
                 </div>
             </Container>
         </Navbar>
