e3dba58305290c223f381928117e85f92a5be7ba
arth1312 <arthkoradiya@gmail.com>
2025-08-28 23:58:47 +0530

    Delete practical-exam directory

--- practical-exam/src/Services/Reducers/index.js
+++
@@ -1,8 +0,0 @@
-import { combineReducers } from "redux";
-import { movieReducer } from "./movieReducer";
-import { userReducer } from "./userReducer";
-
-export const rootReducer = combineReducers({
-    movieReducer,
-    userReducer
-})
